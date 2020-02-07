4fbcc3b67870b2cf9dab596239fe05f3cf939175
cleysonnn <you@example.com>
2020-02-07 17:00:04 -0300

    first commit

---
+++ Tentativa1.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(){
+    cout<<"digite 2 numeros"<< endl ;
+    int n1,n2;
+    cin >> n1 >>n2;
+
+    cout<<"a soma de "<<n1 <<" and "<<n2<<" is "<< n1+n2<<endl;
+    return 0;
+
+}
+
---
+++ Tentativa2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+
+int main(){
+
+    int n;
+
+    for(;;;){
+        std::cin>> n;
+        std::cout<<"ola " << k << "number "<< n << std::endl;
+    }
+
+    return 0;
+}
